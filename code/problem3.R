bd0eab9fdcaf6aba6516162ef5a66afcda402046
Berild <moutzebe@gmail.com>
2019-03-14 18:44:09 +0100

    Add clustering classification

--- code/problem3.R
+++ code/problem3.R
@@ -5,10 +5,11 @@ library(spatial)
 library(tidyverse)
 library(gridExtra)
 library(reshape2)
+library(factoextra)
 library(RColorBrewer)
 
 # read data
-redwood = as.list(read.table('../data_files/redwood.dat', col.names = c('x', 'y')))
+redwood = as.list(read.table('../data/redwood.dat', col.names = c('x', 'y')))
 
 #Display redwood data
 redwood_df = data.frame(x = redwood$x, y = redwood$y)
@@ -17,3 +18,14 @@ ggplot(data = redwood_df, aes(x=x, y=y)) +
   geom_point() + 
   ggtitle("Redwood data") +
   theme(plot.title = element_text(hjust = 0.5))
+
+res.dist <- get_dist(redwood_df,stand = TRUE, method = "pearson")
+fviz_dist(res.dist, 
+          gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+fviz_nbclust(redwood_df, kmeans, method = "gap_stat")
+set.seed(123)
+km.res <- kmeans(redwood_df, 7, nstart = 25)
+a <-fviz_cluster(km.res, data = redwood_df,
+             ellipse.type = "convex",
+             palette = "jco",
+             ggtheme = theme_minimal())
