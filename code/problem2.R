64aacd19bbc8d47ca66f8e686eb3b83cb2e0a74e
Sindre Henriksen <sindrehenriksen93@gmail.com>
2019-05-08 10:30:22 +0200

    Update p2 figure style

--- code/problem2.R
+++ code/problem2.R
@@ -3,6 +3,7 @@ rm(list=ls())
 
 library(tidyverse)
 library(gridExtra)
+set.seed(123)
 
 # Load data
 data = as_tibble(read.table("../data/obsprob.txt", header=TRUE))
@@ -14,12 +15,13 @@ n = nrow(data)
 # Plot number of observed pines and observation probabilities
 p = ggplot(data, aes(x, y))
 fill = scale_fill_distiller(palette='Spectral')
-pines_plot = p + geom_raster(aes(fill=pines)) + fill
-alpha_plot = p + geom_raster(aes(fill=alpha)) + fill
+theme = theme_minimal()
+pines_plot = p + geom_raster(aes(fill=pines)) + fill + theme
+alpha_plot = p + geom_raster(aes(fill=alpha)) + fill + theme
 ggsave("../figures/p2_pines.pdf", plot=pines_plot,
-       width=5, height=4, units="in", dpi=300)
+       width=4.5, height=3.5, units="in", dpi=300)
 ggsave("../figures/p2_alpha.pdf", plot=alpha_plot,
-       width=5, height=4, units="in", dpi=300)
+       width=4.5, height=3.5, units="in", dpi=300)
 
 # Calculate estimate of intensity
 lambda_hat = sum(data$pines / data$alpha) / tot_area
@@ -36,7 +38,7 @@ d = data
 for(i in 1:n_sims){
   d["pines"] = prior_sims[,i]
   prior_sim_plots[[i]] = ggplot(d, aes(x, y)) +
-    geom_raster(aes(fill=pines)) + fill + margin + labs
+    geom_raster(aes(fill=pines)) + fill + theme + margin + labs
 }
 prior_sim_grid_plot = arrangeGrob(grobs=prior_sim_plots, ncol=2)
 ggsave("../figures/p2_prior_sims.pdf", plot=prior_sim_grid_plot,
@@ -60,7 +62,7 @@ posterior_sim_plots = vector("list", n_sims)
 for(i in 1:n_sims){
   d["pines"] = posterior_sims[,i] + data$pines
   posterior_sim_plots[[i]] = ggplot(d, aes(x, y)) +
-    geom_raster(aes(fill=pines)) + fill + margin + labs
+    geom_raster(aes(fill=pines)) + fill + theme + margin + labs
 }
 posterior_sim_grid_plot = arrangeGrob(grobs=posterior_sim_plots, ncol=2)
 ggsave("../figures/p2_posterior_sims.pdf", plot=posterior_sim_grid_plot,
@@ -78,6 +80,6 @@ for(i in 1:n_sims){
 # Plot posterior expected number of pine trees
 d["pines"] = posterior_intensity + data$pines
 posterior_mean_plot = ggplot(data=d, aes(x, y)) +
-  geom_raster(aes(fill=pines)) + fill
+  geom_raster(aes(fill=pines)) + fill + theme
 ggsave("../figures/p2_posterior_mean.pdf", plot=posterior_mean_plot,
        width=5, height=4, units="in", dpi=300)
